4ef01dbebc5d9cc6667ae6d52716e811048169b3
JyriSem <jyrisem@gmail.com>
2024-11-24 19:50:51 +0200

    First

---
+++ src/components/ShoppingCart.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>hahahah</h1>
+    <p>Total</p>
+    <p>${{cartTotal.toFixed(2)}}</p>
+  </div>
+
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: () => ({
+    apibase: "http://localhost:8089/api/cart",
+    cartTotal: 0
+  }),
+  mounted() {
+    axios
+        .get(`${this.apibase}/cart-total`)
+        .then((res) => (this.cartTotal = res.data))
+        .catch(console.error)
+  }
+};
+</script>
+
+<style>
+
+</style>
